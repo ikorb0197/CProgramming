b65c1eb1bf5064d2ab9d4d70e90114c5751c3214
Isaac Korb <ikorb0197@floridapoly.edu>
2025-01-22 04:50:54 +0000

    Get input

--- 03_get_input/get_input.c
+++ 03_get_input/get_input.c
@@ -14,9 +14,11 @@ int main() {
 
     // Do the math
     square_perimeter = square_side * 4;
+    square_area = square_side * square_side;
 
     // Create the output
     printf("The perimeter of a square with side %d is %d\n", square_side, square_perimeter);
+    printf("The area of a square with side %d is %d\n", square_side, square_area);
 
     return 0;
 }
